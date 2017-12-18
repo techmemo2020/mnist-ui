b1e88e4281dc984e16403f16b054278bfc47686d
Makoto Hatanaka <makoto@alumni.caltech.edu>
2017-12-18 22:15:34 +0900

    test

--- hello.py
+++ hello.py
@@ -15,8 +15,10 @@ def hello():
 if __name__ == "__main__":
 	port = os.environ.get("PORT")
 	if port:
-		app.run(host="0.0.0.0",port=port)
-	else
-		app.run(host="0.0.0.0")
+		app.run(port=port)
+		#app.run(host="0.0.0.0",port=port)
+	else:
+		app.run()
+		#app.run(host="0.0.0.0")
 	#app.run(host="0.0.0.0",port=5000,debug=True)
 
